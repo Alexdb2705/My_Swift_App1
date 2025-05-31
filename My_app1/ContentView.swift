d4560560ab455711a6307289aff326e7fca24b05
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-05-31 22:31:30 +0200

    Initial Commit

---
+++ My_app1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My_app1
+//
+//  Created by Alex del Barrio on 31/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My_app1/My_app1App.swift
@@ -0,0 +1,17 @@
+//
+//  My_app1App.swift
+//  My_app1
+//
+//  Created by Alex del Barrio on 31/5/25.
+//
+
+import SwiftUI
+
+@main
+struct My_app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
